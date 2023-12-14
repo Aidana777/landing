fca662a7efb15ac191f4f13fa4279dc4f117a9d3
Aidana777 <aidananiazalieva27@gmail.com>
2023-12-14 22:57:40 +0600

    add

--- src/components/Catheters/Catheters.tsx
+++ src/components/Catheters/Catheters.tsx
@@ -4,29 +4,27 @@ const Catheters = () => {
     return (
         <div>
             <div className="cathetersBlock">
-                <div className="customBlock firstModal">
-                    <div className="customBox first_Modal">
-                        <input className='customInput modalFi' type="text" placeholder='Спр. "Катетеры"' />
-                        <button className='customBtn btn_First'>
-                            <img src="../../../icons/burger-checklist-list-menu-navigation-svgrepo-com.svg" alt="" />
-                        </button>
-                    </div>
 
-                </div>
-
-                <div className="customBlock secondModal">
-                    <div className="customBox Modal_Second">
-                        <input className='customInput modal_Second' type="text" placeholder='Спр. "Типы катетеров"' />
-                        <button className='customBtn btn_Second'>
+                <div className="catheterModalFirst">
+                    <div className="boxCather">
+                        <input className='inputCather' type="text" placeholder=' Спр. "Катетеры"' />
+                        <button className='catherBtn'>
                             <img src="../../../icons/burger-checklist-list-menu-navigation-svgrepo-com.svg" alt="" />
                         </button>
                     </div>
                 </div>
 
-
+                <div className="catheterModalSecond">
+                    <div className="boxCather">
+                        <input className='inputCather' type="text" placeholder='Спр. "Типы катетеров"' />
+                        <button className='catherBtn'>
+                            <img src="../../../icons/burger-checklist-list-menu-navigation-svgrepo-com.svg" alt="" />
+                        </button>
+                    </div>
+                </div>
             </div>
-
         </div>
+
     )
 }
 
