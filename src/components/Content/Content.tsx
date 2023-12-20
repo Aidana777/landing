e7f12d3c7f2f913bdccae5cd8b952af00ea18528
Aidana777 <aidananiazalieva27@gmail.com>
2023-12-20 18:14:50 +0600

    Игла

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,10 +1,8 @@
-// Content.tsx
-import React, { useState } from 'react';
+
 import '../css/style.css'
 import ProgramButtons from './ProgramButtons';
 import ThreeBtnSection from '../ThreeBtnSection/ThreeBtnSection';
 import InjectionSection from '../InjectionSection/InjectionSection';
-import ModalSection from '../ModalSection/ModalSection';
 import Banner from '../Banner/Banner';
 import TextBlock from '../TextBlock/TextBlock';
 import HomeBlock from '../HomeBlock/HomeBlock';
@@ -13,15 +11,13 @@ import { RootState } from '../redux/store';
 import {
   setSelectedProgram,
   setSelectedButton,
-  setIsModalOpen,
-  setItemsModalFirst,
+ 
 
 } from '../Slices/contentSlice';
 
 const Content = () => {
   const dispatch = useDispatch();
   const contentState = useSelector((state: RootState) => state.content);
-  const [inputValueFirst, setInputValueFirst] = useState('');
 
   return (
     <div className="container">
@@ -44,25 +40,7 @@ const Content = () => {
         }}
       />
 
-      <ModalSection
-        isOpen={contentState.isModalOpen}
-        onClose={() => {
-          dispatch(setIsModalOpen(false));
-          // Additional logic if needed
-        }}
-        selectedButton={contentState.selectedButton}
-        needleTypeContent={contentState.needleTypeContent}
-        catheterTypeContent={contentState.catheterTypeContent}
-        itemsModalFirst={contentState.itemsModalFirst}
-        onRemoveItemFirst={(index: number) => dispatch(setItemsModalFirst(contentState.itemsModalFirst.filter((_, i) => i !== index)))}
-        onInputChangeFirst={(e: React.ChangeEvent<HTMLInputElement>) => setInputValueFirst(e.target.value)}
-        onAddItemFirst={() => {
-          if (inputValueFirst.trim() !== '') {
-            dispatch(setItemsModalFirst([...contentState.itemsModalFirst, inputValueFirst]));
-            setInputValueFirst('');
-          }
-        }}
-      />
+
 
      <Banner/>
      <HomeBlock/>
--- src/components/InjectionSection/InjectionContent.tsx
+++ src/components/InjectionSection/InjectionContent.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { setModalContent, setIsModalOpen } from '../Slices/contentSlice';
-import Modal from '../ModalProps/ModalProps';
+
 
 const InjectionContent: React.FC = () => {
   const dispatch = useDispatch();
